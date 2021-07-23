26806285565999c5ba823aa308e33ef44728f25c
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-07-23 07:54:26 +0200

    Update stock_picking.py

--- dropship_edi_integration_ept/models/stock_picking.py
+++ dropship_edi_integration_ept/models/stock_picking.py
@@ -479,9 +479,9 @@ class StockPicking(models.Model):
                 self._create_common_log_line(job, csvwriter, log_message)
                 
                 return True
-            #if id_temp2:
-                #stock_move_line_old_id.id = id_temp2
-            #stock_move_line_import_id.id = id_temp1
+            if id_temp2:
+                stock_move_line_old_id.id = id_temp2
+            stock_move_line_import_id.id = id_temp1
 
             stock_move_line_old_id.importednum = True
             return True
